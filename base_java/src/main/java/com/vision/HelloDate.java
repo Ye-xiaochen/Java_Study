60ced8aac0f02409e8efdb027a4dfbcaf553b871
yexiaochen <Yxc10769@163.com>
2022-09-14 22:50:44 +0800

    调整结构

---
+++ base_java/src/main/java/com/vision/HelloDate.java
@@ -0,0 +1,17 @@
+package com.vision;
+
+import java.util.*;
+
+/**
+ * @author yxc
+ */
+public class HelloDate {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, it's:   ");
+        System.out.println(new Date());
+    }
+
+}
+
